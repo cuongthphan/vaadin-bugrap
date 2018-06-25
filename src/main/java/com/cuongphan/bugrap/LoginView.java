b1f01f23f8368042f4716138163ae7bbfe57eee8
Cuong Phan <cuong@vaadin.com>
2018-06-25 11:01:24 +0300

    add enter shortcut to login form

--- src/main/java/com/cuongphan/bugrap/LoginView.java
+++ src/main/java/com/cuongphan/bugrap/LoginView.java
@@ -2,14 +2,17 @@ package com.cuongphan.bugrap;
 
 import com.cuongphan.bugrap.ui.MainUI;
 import com.cuongphan.bugrap.utils.Account;
-import com.cuongphan.bugrap.utils.ReportFilter;
 import com.cuongphan.bugrap.utils.ViewNames;
 import com.vaadin.data.*;
 import com.vaadin.data.validator.EmailValidator;
+import com.vaadin.event.ShortcutListener;
 import com.vaadin.navigator.View;
+import com.vaadin.shared.Registration;
 import com.vaadin.ui.UI;
 import org.vaadin.bugrap.domain.BugrapRepository;
 import org.vaadin.bugrap.domain.entities.Reporter;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 public class LoginView extends LoginDesign implements View {
 
@@ -19,6 +22,7 @@ public class LoginView extends LoginDesign implements View {
         BugrapRepository bugrapRepository = new BugrapRepository("/Users/cuongphanthanh/bugrap-database");
         bugrapRepository.populateWithTestData();
         Account account = new Account();
+        final Registration[] reg = new Registration[3];
 
         BindingValidationStatusHandler statusHandler = new BindingValidationStatusHandler() {
             @Override
@@ -35,6 +39,38 @@ public class LoginView extends LoginDesign implements View {
                 .bind(Account::getEmail, Account::setEmail);
         accountBinder.forField(passwordField)
                 .bind(Account::getPassword, Account::setPassword);
+
+        ShortcutListener shortcut =
+                new ShortcutListener("Enter", ShortcutListener.KeyCode.ENTER, null) {
+            @Override
+            public void handleAction(Object sender, Object target) {
+                loginButton.click();
+            }
+        };
+
+        emailTF.addFocusListener(event -> {
+            reg[0] = emailTF.addShortcutListener(shortcut);
+        });
+
+        passwordField.addFocusListener(event -> {
+           reg[1] = passwordField.addShortcutListener(shortcut);
+        });
+
+        loginButton.addFocusListener(event -> {
+            reg[2] = loginButton.addShortcutListener(shortcut);
+        });
+
+        emailTF.addBlurListener(event -> {
+            reg[0].remove();
+        });
+
+        passwordField.addBlurListener(event -> {
+            reg[1].remove();
+        });
+
+        loginButton.addBlurListener(event -> {
+            reg[2].remove();
+        });
 
         loginButton.addClickListener(event -> {
             if (!accountBinder.isValid()) {
